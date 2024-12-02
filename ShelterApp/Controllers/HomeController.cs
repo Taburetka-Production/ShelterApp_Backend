e3186bf7e7a6ecf5c897a219ac1a2b38dd33b485
Andrew1369 <157983860+Andrew1369@users.noreply.github.com>
2024-12-02 16:42:21 +0200

    Add project files.

---
+++ ShelterApp/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShelterApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
