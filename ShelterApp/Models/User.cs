6b4658250782d4a1e2c9e555b2e807b32e06e7b9
Andrew1369 <157983860+Andrew1369@users.noreply.github.com>
2024-12-04 21:29:05 +0200

    for HomeController

---
+++ ShelterApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelterApp.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Role { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
