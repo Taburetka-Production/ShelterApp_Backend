22d0f133ccfa0abca9983adff36defdc0415cc9d
bohdan-vykhovanets <bohdan.vykhovanets@gmail.com>
2025-01-09 05:24:22 +0200

    Added ShelterApp.Tests project and unit-test for SheltersController

---
+++ ShelterApp.Tests/TestsSetup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShelterApp.Tests
+{
+    public class TestsSetup
+    {
+        public ApplicationDbContext GetInMemoryDbContext()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite(connection).Options;
+
+            var dbContext = new ApplicationDbContext(options);
+
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+    }
+}
