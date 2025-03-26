743b26f8f1f0d2f08168a18f66d79deab0caced3
Andrew1369 <157983860+Andrew1369@users.noreply.github.com>
2025-03-26 23:37:32 +0200

    Перекинув дії з інформацією про юзера в акаунт контролер

--- ShelterApp/Controllers/UserController.cs
+++ ShelterApp/Controllers/UserController.cs
@@ -17,61 +17,6 @@ namespace ShelterApp
             _userManager = userManager;
         }
 
-        [HttpGet("info")]
-        public async Task<IActionResult> GetUserInfo()
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-            if (string.IsNullOrEmpty(userId))
-            {
-                return BadRequest("User identifier not found in claims.");
-            }
-
-            var user = await _userManager.FindByIdAsync(userId);
-
-            if (user == null)
-            {
-                return NotFound("User not found.");
-            }
-
-            var userInfo = new
-            {
-                user.UserName,
-                user.Name,
-                user.Surname,
-                user.Email,
-                user.AvatarUrl,
-                user.Age,
-                user.PhoneNumber
-            };
-
-            return Ok(userInfo);
-        }
-
-        [HttpPut("info")]
-        public async Task<IActionResult> UpdateUserInfo([FromBody] UpdateUserDto updatedUserDto)
-        {
-            var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
-            if (userId == null) return Unauthorized("User not found");
-
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null) return NotFound("User not found");
-
-            user.UserName = updatedUserDto.UserName ?? user.UserName;
-            user.Name = updatedUserDto.Name ?? user.Name;
-            user.Surname = updatedUserDto.Surname ?? user.Surname;
-            user.Age = updatedUserDto.Age ?? user.Age;
-            user.PhoneNumber = updatedUserDto.PhoneNumber ?? user.PhoneNumber;
-            user.AvatarUrl = updatedUserDto.AvatarUrl ?? user.AvatarUrl;
-
-            var result = await _userManager.UpdateAsync(user);
-            if (!result.Succeeded)
-            {
-                return BadRequest(result.Errors);
-            }
-
-            return NoContent();
-
-        }
+        
     }
 }
