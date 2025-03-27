d167b8c38688ab24e24886cdc4d33fd8205e6daf
Andrew1369 <157983860+Andrew1369@users.noreply.github.com>
2025-03-28 01:44:30 +0200

    Створив функції видалення юзерів та адмінів, заапгрейдити юзера до адміна, виведення юзерів та адмінів

--- ShelterApp/Controllers/UserController.cs
+++ ShelterApp/Controllers/UserController.cs
@@ -7,7 +7,7 @@ namespace ShelterApp
 {
     [ApiController]
     [Route("api/[controller]")]
-    [Authorize]
+    [Authorize(Roles = "Superadmin")]
     public class UsersController : ControllerBase
     {
         private readonly UserManager<User> _userManager;
@@ -17,6 +17,69 @@ namespace ShelterApp
             _userManager = userManager;
         }
 
+        [HttpGet("all-users")]
         
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var allUsers = _userManager.Users.ToList();
+            var usersWithRoles = new List<object>();
+
+            foreach (var user in allUsers)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                // Пропускаємо користувачів з роллю Superadmin
+                if (roles.Contains("Superadmin"))
+                {
+                    continue;
+                }
+
+                usersWithRoles.Add(new
+                {
+                    user.Id,
+                    user.AvatarUrl,
+                    user.Name,
+                    user.Surname,
+                    user.Age,
+                    user.Email,
+                    Roles = roles
+                });
+            }
+
+            return Ok(usersWithRoles);
+        }
+
+        [HttpDelete("{id}")]
+        
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+
+            var roles = await _userManager.GetRolesAsync(user);
+            if (roles.Contains("Superadmin") || roles.Contains("ShelterAdmin"))
+            {
+                return BadRequest("Cannot delete admin users.");
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+            return result.Succeeded ? NoContent() : BadRequest(result.Errors);
+        }
+
+        [HttpPost("grant-admin/{id}")]
+        
+        public async Task<IActionResult> GrantAdmin(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+
+            if (await _userManager.IsInRoleAsync(user, "ShelterAdmin"))
+            {
+                return BadRequest("User is already an admin.");
+            }
+
+            var result = await _userManager.AddToRoleAsync(user, "ShelterAdmin");
+            return result.Succeeded ? Ok() : BadRequest(result.Errors);
+        }
     }
 }
