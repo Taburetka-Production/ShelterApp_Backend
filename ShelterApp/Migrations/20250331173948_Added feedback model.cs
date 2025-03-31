7415ac4e44e0857e157d5848fb2e3dbc12645afe
dro1d <andriyzaharchenko@gmail.com>
2025-03-31 20:40:50 +0300

    Feedback model added

---
+++ ShelterApp/Migrations/20250331173948_Added feedback model.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShelterApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedfeedbackmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
